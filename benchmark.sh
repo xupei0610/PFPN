c3310fee52898ea9a3c0eb485942e5c1fd7ebfe2
Pei Xu <xupei0610@PeiXuArch.localdomain>
2021-10-19 07:41:30 -0400

    remove unused envs

--- benchmark.sh
+++ benchmark.sh
@@ -7,16 +7,8 @@ par=$3
 suf=${@:5}
 run()
 {
-    if [[ "$env" == "Ant"* ]]; then
-        SEEDS=( 13248 16542 17648 25911 30624 )
-    elif [[ "$env" == "HalfCheetah"* ]]; then
-        SEEDS=( 2533 9950 15509 15696 15737 )
-    elif [[ "$env" == "Humanoid"* ]]; then
-        SEEDS=( 38675 4975 37471 14464 19509 )
-    elif [[ "$env" == "DeepMimic"* ]]; then
+    if [[ "$env" == "DeepMimic"* ]]; then
         SEEDS=( 34114 33406 28949 12831 39907 )
-    elif [[ "$env" == "Reacher"* ]]; then
-        SEEDS=( 31852 30901 32503 32593 3718 )
     else
         echo "Unkown Env: ${env}"
         exit
